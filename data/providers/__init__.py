c87d429ea8e51b33d3ecd66a950e7f581f5448e1
siasoltoon <siyavashjahangiri105@gmail.com>
2026-09-19 18:41:14 +0330

    feat(market-data): export Binance provider

--- data/providers/__init__.py
+++ data/providers/__init__.py
@@ -1,10 +1,12 @@
 from data.providers.oanda_provider import OandaProvider
 from data.providers.finnhub_provider import FinnhubProvider
 from data.providers.alphavantage_provider import AlphaVantageProvider
+from data.providers.binance_provider import BinanceProvider
 
 
 __all__ = [
     "OandaProvider",
     "FinnhubProvider",
     "AlphaVantageProvider",
+    "BinanceProvider",
 ]
