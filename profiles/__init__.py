7ee70ad23a7b5c0008298e37f3286d1be4869853
siasoltoon <siyavashjahangiri105@gmail.com>
2026-09-20 16:22:16 +0330

    fix(profiles): export ProfileStyle for execution contracts

--- profiles/__init__.py
+++ profiles/__init__.py
@@ -1,8 +1,8 @@
-from .models import AnalysisProfile, PRESET_PROFILES, profile_from_preset
+from .models import AnalysisProfile, ProfileStyle, PRESET_PROFILES, profile_from_preset
 from .service import create_profile, get_profile, list_profiles, set_active_profile, update_profile
 
 __all__ = [
-    "AnalysisProfile", "PRESET_PROFILES", "profile_from_preset",
+    "AnalysisProfile", "ProfileStyle", "PRESET_PROFILES", "profile_from_preset",
     "create_profile", "get_profile", "list_profiles", "set_active_profile", "update_profile",
     "ProfileExecutionContext", "build_execution_context", "context_from_payload", "context_payload", "profile_snapshot",
 ]
