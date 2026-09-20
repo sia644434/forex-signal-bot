4dabc99579d709e0989cd797dfaf63870065a830
siasoltoon <siyavashjahangiri105@gmail.com>
2026-09-20 16:04:13 +0330

    chore(profiles): export execution context symbols

--- profiles/__init__.py
+++ profiles/__init__.py
@@ -4,6 +4,7 @@ from .service import create_profile, get_profile, list_profiles, set_active_prof
 __all__ = [
     "AnalysisProfile", "PRESET_PROFILES", "profile_from_preset",
     "create_profile", "get_profile", "list_profiles", "set_active_profile", "update_profile",
+    "ProfileExecutionContext", "build_execution_context", "context_from_payload", "profile_snapshot",
 ]
 
 from .execution import ProfileExecutionContext, build_execution_context, context_from_payload, profile_snapshot
