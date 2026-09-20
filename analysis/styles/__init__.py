61595cbf46e4f7b2e3ce53e3a0d118d7313c6b8b
siasoltoon <siyavashjahangiri105@gmail.com>
2026-09-20 15:47:51 +0330

    feat(profiles): add profile model and persistence service

---
+++ analysis/styles/__init__.py
@@ -0,0 +1,3 @@
+from .registry import AnalysisStyle, get_analysis_style, list_analysis_styles, list_preset_styles
+
+__all__ = ["AnalysisStyle", "get_analysis_style", "list_analysis_styles", "list_preset_styles"]
